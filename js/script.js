18a607b4b8b39b2fea256dab4cbd3e82599756f5
Bartosz Głodek <bartoszglodek@wp.pl>
2023-05-03 14:06:11 +0200

    Add complete titleClickHandler

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 'use strict';
 
 function titleClickHandler(event) {
+    event.preventDefault();
+    const clickedElement = this
+
     console.log('Link was clicked!');
     console.log(event);
 
-//-------------------------------------------------------------------------------------------------
-
-    /* remove class 'active' from all article links  */
+//-----------------------------------------------------Remove class 'active' from all article links-----
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -14,15 +15,14 @@ function titleClickHandler(event) {
         activeLink.classList.remove('active');
     }
 
-//-------------------------------------------------------------------------------------------------
 
-    /* add class 'active' to the clicked link */
+//-----------------------------------------------------------Add class 'active' to the clicked link-----
 
-  
+    this.classList.add('active');
+    console.log('clickedElement:', clickedElement);
 
-//-------------------------------------------------------------------------------------------------
 
-    /* remove class 'active' from all articles */
+//----------------------------------------------------------Remove class 'active' from all articles-----
 
     const activeArticles = document.querySelectorAll('.posts .active');
 
@@ -30,18 +30,22 @@ function titleClickHandler(event) {
         activeArticle.classList.remove('active');
     }
 
-//-------------------------------------------------------------------------------------------------
 
-    /* get 'href' attribute from the clicked link */
+//-------------------------------------------------------Get 'href' attribute from the clicked link-----
 
-//-------------------------------------------------------------------------------------------------
+    const articleSelector = this.getAttribute('href');
 
-    /* find the correct article using the selector (value of 'href' attribute) */
 
-//-------------------------------------------------------------------------------------------------
+//--------------------------Find the correct article using the selector (value of 'href' attribute)-----
 
-    /* add class 'active' to the correct article */
+    const targetArticle = document.querySelector(articleSelector);
+
+
+//--------------------------------------------------------Add class 'active' to the correct article-----
+
+    targetArticle.classList.add('active');
 }
+
 
 const links = document.querySelectorAll('.titles a');
 
