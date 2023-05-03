4ea821eb9ee1f4f0be9c64ed3cce8000b4456285
Bartosz Głodek <bartoszglodek@wp.pl>
2023-05-03 12:25:15 +0200

    Added removing classes by JS

--- js/script.js
+++ js/script.js
@@ -1,24 +1,50 @@
 'use strict';
 
-function titleClickHandler(event){
-  console.log('Link was clicked!');
-  console.log(event);
+function titleClickHandler(event) {
+    console.log('Link was clicked!');
+    console.log(event);
 
-  /* remove class 'active' from all article links  */
+//-------------------------------------------------------------------------------------------------
 
-  /* add class 'active' to the clicked link */
+    /* remove class 'active' from all article links  */
 
-  /* remove class 'active' from all articles */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* get 'href' attribute from the clicked link */
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+//-------------------------------------------------------------------------------------------------
 
-  /* add class 'active' to the correct article */
+    /* add class 'active' to the clicked link */
+
+  
+
+//-------------------------------------------------------------------------------------------------
+
+    /* remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.posts .active');
+
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
+
+//-------------------------------------------------------------------------------------------------
+
+    /* get 'href' attribute from the clicked link */
+
+//-------------------------------------------------------------------------------------------------
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+//-------------------------------------------------------------------------------------------------
+
+    /* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
 }
