c81a359a220363cb23220ef10053dabfaf726c66
Bartosz Głodek <bartoszglodek@wp.pl>
2023-05-05 09:22:45 +0200

    What now?

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function titleClickHandler(event) {
     console.log('Link was clicked!');
     console.log(event);
 
-//-----------------------------------------------------Remove class 'active' from all article links-----
+    //-----------------------------------------------------Remove class 'active' from all article links-----
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -16,13 +16,13 @@ function titleClickHandler(event) {
     }
 
 
-//-----------------------------------------------------------Add class 'active' to the clicked link-----
+    //-----------------------------------------------------------Add class 'active' to the clicked link-----
 
     this.classList.add('active');
     console.log('clickedElement:', clickedElement);
 
 
-//----------------------------------------------------------Remove class 'active' from all articles-----
+    //----------------------------------------------------------Remove class 'active' from all articles-----
 
     const activeArticles = document.querySelectorAll('.posts .active');
 
@@ -31,17 +31,17 @@ function titleClickHandler(event) {
     }
 
 
-//-------------------------------------------------------Get 'href' attribute from the clicked link-----
+    //-------------------------------------------------------Get 'href' attribute from the clicked link-----
 
     const articleSelector = this.getAttribute('href');
 
 
-//--------------------------Find the correct article using the selector (value of 'href' attribute)-----
+    //--------------------------Find the correct article using the selector (value of 'href' attribute)-----
 
     const targetArticle = document.querySelector(articleSelector);
 
 
-//--------------------------------------------------------Add class 'active' to the correct article-----
+    //--------------------------------------------------------Add class 'active' to the correct article-----
 
     targetArticle.classList.add('active');
 }
@@ -52,3 +52,75 @@ const links = document.querySelectorAll('.titles a');
 for (let link of links) {
     link.addEventListener('click', titleClickHandler);
 }
+
+
+//___LIST GENERATION FUNCTION_______________________________________________________________________________
+
+const 
+  optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+  
+    /* remove contents of titleList */
+
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+
+    /* ... */
+
+
+
+    /* find all the articles and save them to variable: articles */
+    
+    let articles = document.querySelectorAll(optArticleSelector);
+
+
+    /* ... */
+
+    let html = '';
+
+    for(let article of articles){
+    
+        /* get the article id */
+    
+        const articleId = article.getAttribute('id');
+
+        /* ... */
+
+    
+        
+        /* find the title element */
+    
+        const articleTitle = article.querySelector(optTitleSelector);
+        
+        /* ... */
+
+    
+        
+        /* get the title from the title element */
+    
+        const titleText = articleTitle.innerHTML;
+        
+        /* ... */
+
+    
+        
+        /* create HTML of the link */
+    
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + titleText + '</span></a></li>';
+        
+        /* ... */
+
+        titleList.insertAdjacentHTML('beforeend', linkHTML);
+        
+        /* insert link into html variable */
+    
+        html = html + linkHTML;
+  }
+
+  titleList.innerHTML = html;
+}
+
+generateTitleLinks();
